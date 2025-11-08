d9c8e63a3faba82fcbc5135aff01eeb27d4ef2b5
Eddy-dev0 <121190497+Eddy-dev0@users.noreply.github.com>
2025-11-08 21:39:41 +0100

    Add layer segmentation and compositing helpers

--- painterslicer/image_analysis/__init__.py
+++ painterslicer/image_analysis/__init__.py
@@ -1,19 +1,24 @@
 """Image analysis helpers used by PainterSlicer."""
 
-from .analyzer import ImageAnalyzer
+from .analyzer import ImageAnalyzer, segment_image_into_layers
 from .pipeline import (
     CalibrationProfile,
     PaletteResult,
     PaintingPipeline,
     PipelineResult,
     StrokeInstruction,
+    enhance_layer,
 )
+from .layer_superres import compose_layers
 
 __all__ = [
     "ImageAnalyzer",
+    "segment_image_into_layers",
     "CalibrationProfile",
     "PaletteResult",
     "PaintingPipeline",
     "PipelineResult",
     "StrokeInstruction",
+    "enhance_layer",
+    "compose_layers",
 ]
